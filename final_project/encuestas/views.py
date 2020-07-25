8f3ea74282447a5a11ad2d2c488ece895d3b7f54
AlexWilliams <aolaechea@unsa.edu.pe>
2020-07-24 21:04:15 -0500

    Se arreglo los conflictos en las views

--- final_project/encuestas/views.py
+++ final_project/encuestas/views.py
@@ -1,20 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-<<<<<<< HEAD
 
-def encuestaCreateView(request):
-    form = encuestaForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = FotoForm()
-
-    context = {
-            'form': form
-            }
-    return render(request, 'encuestas/encuestaCreate.html', context)
-
-=======
 def home(request):
 	return HttpResponse("Aqui estara nuestro home")
 def detalle(request):
@@ -23,4 +10,3 @@ def tablon(request):
 	return HttpResponse("Aqui se mostraran la primeras cinco preguntas publicadas")
 def editar(request):
 	return HttpResponse("aqui se editara la pregunta")
->>>>>>> origin/experimental
