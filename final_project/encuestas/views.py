771e5627101502021cc8dc6403d244dd32da66a8
AlexWilliams <aolaechea@unsa.edu.pe>
2020-07-24 20:52:14 -0500

    Se creo la vista para crear encuestas

--- final_project/encuestas/views.py
+++ final_project/encuestas/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def encuestaCreateView(request):
+    form = encuestaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = FotoForm()
+
+    context = {
+            'form': form
+            }
+    return render(request, 'encuestas/encuestaCreate.html', context)
+
