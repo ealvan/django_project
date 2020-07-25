7857f1d412ba071cf01815e114a331b334690ef0
EDSEL <ealvan@unsa.edu.pe>
2020-07-25 15:48:41 -0500

    creando mis vistas reltados, votar, y detalle

--- final_project/encuestas/views.py
+++ final_project/encuestas/views.py
@@ -1,11 +1,40 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from encuestas.models import Pregunta,Opcion
+from django.urls import reverse
 # Create your views here.
 
 def home(request):
 	return HttpResponse("Aqui estara nuestro home")
-def detalle(request):
-	return HttpResponse("Aqui estara la pregunta publicada")
+def detalle(request,preguntaID):
+	try:
+		pregunta = Pregunta.objects.get(pk=preguntaID)
+	except:
+		raise Http404("la pregunta no existe")
+	return render(request,'encuestas/detalle.html',{'pregunta':pregunta})
+def votar(request,preguntaID):
+	try:
+		pregunta = Pregunta.objects.get(pk=preguntaID)
+	except:
+		raise Http404("la pregunta no existe")
+	try:
+		opcion_selec = pregunta.opcion_set.get(pk=request.POST['opcion'])
+	except:
+		diccionario = {'pregunta':pregunta, 'error':'tu no seleccionaste una opcion'}
+		return render(request,'encuestas/detalle.html',diccionario)
+	else:
+		opcion_selec.votos += 1
+		opcion_selec.save()
+		return HttpResponseRedirect(reverse('encuestas:resultado',args=(pregunta.id,)))
+
+def resultado(request,preguntaID):
+	try:
+		pregunta = Pregunta.objects.get(pk = preguntaID)
+	except:
+		raise Http404("la pregunta no existe")
+	return render(request,'encuestas/resultado.html',{'pregunta':pregunta})
+
+
 def tablon(request):
 	return HttpResponse("Aqui se mostraran la primeras cinco preguntas publicadas")
 def editar(request):
