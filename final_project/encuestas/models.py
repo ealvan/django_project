706ddbff6f2a99c7576b8bf226222c6987707f84
EDSEL <ealvan@unsa.edu.pe>
2020-07-24 20:24:30 -0500

    creando modelos y haciendop  migraciones

--- final_project/encuestas/models.py
+++ final_project/encuestas/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.utils import timezone
+import datetime
 # Create your models here.
+class Pregunta(models.Model):
+	pregunta_txt = models.CharField(max_length=100)
+	pub_fecha  = models.DateTimeField('Fecha de Publicacion')
+	def __str__(self):
+		return f'{self.pregunta_txt}'
+
+class Opcion(models.Model):
+	pregunta_txt = models.ForeignKey(Pregunta, on_delete = models.CASCADE)
+	opcion_txt = models.CharField(max_length=100)
+	votos = models.IntegerField(default=0)	
+	def __str__(self):
+		return f'opinion: {self.opcion_txt} votos: {self.votos}'
