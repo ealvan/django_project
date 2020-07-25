e8cd6400f67a41598528ee7b005152380ba1baf7
EDSEL <ealvan@unsa.edu.pe>
2020-07-25 15:49:19 -0500

    enrutamiento de mis views, con los parametros

--- final_project/encuestas/urls.py
+++ final_project/encuestas/urls.py
@@ -5,8 +5,10 @@ app_name = 'encuestas'
 
 urlpatterns = [
 	path('', views.home, name='home'),
-	path('detalle/',views.detalle, name="detalle"),
+	path('<int:preguntaID>/',views.detalle, name="detalle"),
 	path('tablon/',views.tablon, name = 'tablon'),
 	path('editar/', views.editar, name = 'editar'),
+	path('<int:preguntaID>/votar/',views.votar,name = 'votar'),
+	path('<int:preguntaID>/resultado/',views.resultado,name='resultado')
 	
 ]
