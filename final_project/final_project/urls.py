24b9b5aa7f1f19fa64c50996f1f291ba67839498
EDSEL <ealvan@unsa.edu.pe>
2020-07-27 21:53:22 -0500

    enrutando la vista registro

--- final_project/final_project/urls.py
+++ final_project/final_project/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from encuestas import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('encuestas/',include('encuestas.urls')),
+    path('registro/',views.registro, name='registro'),
 ]
