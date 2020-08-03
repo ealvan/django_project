460f2d078524d8f807e39aebb9089ae28af9208e
EDSEL <ealvan@unsa.edu.pe>
2020-08-02 22:15:30 -0500

    solucion a no legible data en el template tablon.html

--- final_project/encuestas/models.py
+++ final_project/encuestas/models.py
@@ -3,13 +3,6 @@ from django.utils import timezone
 # Create your models here.
 class Pregunta(models.Model):
     pregunta_txt = models.CharField(max_length=100)
-    comida='co'
-    deporte='dp'
-    salud='sa'
-    farandula='fa'
-    noticias='no'
-    cine='cn'
-    ciencias='ci'
     opciones = [('comida','Comida'),
     			('deporte','Deporte'),
     			('salud','Salud'),
@@ -18,7 +11,7 @@ class Pregunta(models.Model):
     			('cine','Cine'),
     			('ciencias','Ciencias'),
             	]
-    area = models.CharField(max_length=2,choices=opciones,default='salud',)
+    area = models.CharField(max_length=20,choices=opciones,default='salud',)
     pub_fecha = models.DateTimeField('Fecha de Publicacion', default = timezone.now())
     def __str__(self):
         return f'{self.pregunta_txt}'
