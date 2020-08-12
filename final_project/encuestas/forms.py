50cb15e88ba4961e510556be01338d059a9fc6b4
EDSEL <ealvan@unsa.edu.pe>
2020-08-11 22:53:57 -0500

    metodo clean_name para limpiar el campo pregunta_txt

--- final_project/encuestas/forms.py
+++ final_project/encuestas/forms.py
@@ -6,28 +6,33 @@ from django.contrib.auth.models import User
 
 class PreguntaForm(forms.ModelForm):
   class Meta:
-    model = Pregunta
-    fields = [
-        'pregunta_txt',
-        'area',
-        ]
+    model=Pregunta 
+    fields = ['pregunta_txt','area']
     widgets = {
-			'pregunta_txt':forms.TextInput(attrs={'class':'special',
-											'placeholder':'ingrese su pregunta'}),
-		}
-	
+            'pregunta_txt':forms.TextInput(attrs={'class':'special',
+                                            'placeholder':'ingrese su pregunta'}),
+        }
+  def clean_pregunta_txt(self):
+    ''' hara una validacion, si no hay un '?' o '¿' en la pregunta'''
+    pregunta = self.cleaned_data.get("pregunta_txt")
+    fin = pregunta[-1]
+    ini = pregunta[0]
+    if fin != '?' or ini != '¿':
+       raise forms.ValidationError("una pregunta debe tener un '¿' y '?'")
+    return pregunta
+    
 class CrearUsuarioForm(UserCreationForm):
-	class Meta:
-		model = User
-		fields = [
-		'username',
-		'email',
-		'password1',
-		'password2',
-		]
-		widgets = {
-			'username':forms.TextInput(attrs={'class':'special',
-											'placeholder':'ingrese su usuario'}),
-			'email':forms.EmailInput(attrs={'class':'special',
-											'placeholder':'ingrese su email'}),
-		}
+    class Meta:
+        model = User
+        fields = [
+        'username',
+        'email',
+        'password1',
+        'password2',
+        ]
+        widgets = {
+            'username':forms.TextInput(attrs={'class':'special',
+                                            'placeholder':'ingrese su usuario'}),
+            'email':forms.EmailInput(attrs={'class':'special',
+                                            'placeholder':'ingrese su email'}),
+        }
